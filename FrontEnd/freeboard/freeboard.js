ca38ae028afc92bed1fae93c873e072996f4822a
ahyeona <ahhyeon741@gmail.com>
2023-06-09 11:58:19 +0900

    freeboard 페이지네이션 추가

---
+++ FrontEnd/freeboard/freeboard.js
@@ -0,0 +1,123 @@
+let list;
+let pagenum;
+let lastPageNum;
+
+window.onload = ()=>{
+    // 서버에서 자유게시판 리스트 받아오는 함수
+
+    // 임의로 리스트 정의
+    list = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+
+    // 첫 페이지는 1번
+    pagenum = 1;
+
+    // 이전/다음 버튼 이벤트 등록
+    addBtnsEvent();
+
+    // 페이지네이션 함수
+    pagenate(pagenum);
+}
+
+// list는 자유게시판 리스트
+
+// 페이지네이션
+const pagenate = (pagenum) => {
+    const pageCount = 10;
+    const length = list.length;
+
+    lastPageNum  = Math.ceil(length/pageCount);
+    const lastBoard = length%pageCount;
+
+    let first = (pagenum-1) * pageCount; // 첫번째로 렌더될 배열 요소의 인덱스
+    let last = pagenum * pageCount; // 마지막로 렌더될 배열 요소의 인덱스 + 1
+
+    // 마지막 페이지일때
+    if(pagenum == lastPageNum && lastBoard != 0){
+        last = first + lastBoard;
+    }
+    freeboards.innerHTML = "";
+
+    for (let index = first; index < last; index++) {
+        // 이런식으로 list에서 인덱스값으로 board 뽑아와야함
+        // const board = list[index];
+
+        // 임의로 값 정함. board.title 이런식으로 정의해야함
+        const title = "제목1";
+        const nickname = "닉네임";
+        const likes = 15;
+        const views = 100;
+        const createdAt = "2023-06-07";
+
+        freeboards.innerHTML +=
+        `<tr>
+            <td>${index}</td>
+            <td class="title"><a href="">${title}</a></td>
+            <td class="nickname">${nickname}</td>
+            <td class="likes">${likes}</td>
+            <td class="views">${views}</td>
+            <td class="createdAt">${createdAt}</td>
+        </tr>`;
+    }
+
+    pageNumBtns.innerHTML = "";
+
+    prevBtn.style.visibility = "visible";
+    nextBtn.style.visibility = "visible";
+
+    // 현재 첫번째 페이지면 이전버튼 안나타나게
+    if (pagenum == 1) {
+        prevBtn.style.visibility = "hidden";
+    }
+
+    // 현재 마지막 페이지면 다음버튼 안나타나게
+    if (pagenum == lastPageNum) {
+        nextBtn.style.visibility = "hidden";
+    }
+
+    for (let index = 1; index < lastPageNum+1; index++) {
+        const pageBtn = document.createElement("div");
+        pageBtn.classList.add("pageBtn");
+        pageBtn.classList.add("btn");
+
+        // 현재 페이지 버튼이면
+        if (index == pagenum) {
+            pageBtn.classList.add("current");
+        }
+
+        pageBtn.innerText = index;
+
+        pageBtn.onclick = () => {
+            pagenate(index);
+        };
+
+        pageNumBtns.append(pageBtn);
+    }
+}
+
+// 다음, 이전 버튼에 이벤트 추가
+const addBtnsEvent = () => {
+    prevBtn.onclick = () => {
+        goPrev();
+    };
+    nextBtn.onclick = () => {
+        goNext();
+    };
+}
+
+// 다음 페이지
+const goPrev = () => {
+    pagenum -= 1;
+    if (pagenum < 1) {
+        pagenum = 1;
+    }
+    pagenate(pagenum);
+}
+
+// 이전 페이지
+const goNext = () => {
+    pagenum += 1;
+    if (pagenum > lastPageNum) {
+        pagenum = lastPageNum;
+    }
+    pagenate(pagenum);
+}
