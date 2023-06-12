bac6472a6762c591bd78c9ac7878136690571eeb
zerohoney <zerohoney97@gmail.com>
2023-06-12 16:24:08 +0900

    addMain4

--- BackEnd/routers/main.js
+++ BackEnd/routers/main.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.sendFile("FrontEnd/mainpage/mainpage.html");
+  res.sendFile("/FrontEnd/mainpage/mainpage.html");
 });
 
 module.exports=router
