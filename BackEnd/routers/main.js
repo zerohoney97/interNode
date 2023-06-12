06ec5392a520ab09e481a45df81bbb75ab094605
zerohoney <zerohoney97@gmail.com>
2023-06-12 16:27:10 +0900

    addMain5

--- BackEnd/routers/main.js
+++ BackEnd/routers/main.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.sendFile("/FrontEnd/mainpage/mainpage.html");
+  res.sendFile("/home/ubuntu/FrontEnd/mainpage/mainpage.html");
 });
 
 module.exports=router
