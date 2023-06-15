6e9a1bb25d3d9914b8980f8bf1f3132b01e6b692
zerohoney <zerohoney97@gmail.com>
2023-06-15 17:22:33 +0900

    option

--- BackEnd/middleware/islogin.js
+++ BackEnd/middleware/islogin.js
@@ -6,6 +6,7 @@ exports.isLogin = (req, res, next) =>{
 
     jwt.verify(access_token, process.env.ACCESS_TOKEN_KEY,(err, acc_decoded)=>{
         if(err){
+            // 다음 미들웨어로 보내짐
             res.send("다시 로그인 해주세요");
         }else{
             req.acc_decoded = acc_decoded;
@@ -14,4 +15,3 @@ exports.isLogin = (req, res, next) =>{
         }
     })
 }
-// 덩산의 코드 훔쳐가요
