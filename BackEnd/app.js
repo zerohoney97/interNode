9a5c9e4df0f58b2b3251ab5660010f7c9fe4996c
zerohoney <zerohoney97@gmail.com>
2023-06-16 09:52:51 +0900

    good4

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -27,7 +27,6 @@ app.use(
     origin: [
       "http://127.0.0.1:5500",
       "http://127.0.0.1:8080",
-      "http://localhost:8080",
       "http://ec2-52-79-43-68.ap-northeast-2.compute.amazonaws.com:8080",
       "http://52.79.43.68:8080",
     ],
