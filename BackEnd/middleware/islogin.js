f482a7277c04f6c6f590ac312aad8bb0ae8ff2b1
zerohoney <zerohoney97@gmail.com>
2023-06-20 15:01:11 +0900

    asd

--- BackEnd/middleware/islogin.js
+++ BackEnd/middleware/islogin.js
@@ -1,18 +1,16 @@
 const jwt = require("jsonwebtoken");
 
-exports.isLogin = (req, res, next) => {
-  const { access_token } = req.session;
-  console.log(access_token, "로그인 미들웨어 거침");
+exports.isLogin = (req, res, next) =>{
+    const {access_token} = req.session;
+    console.log(access_token);
 
-  jwt.verify(access_token, process.env.ACCESS_TOKEN_KEY, (err, acc_decoded) => {
-    if (err) {
-      console.log(err);
-
-      res.send("다시 로그인 해주세요");
-    } else {
-      req.acc_decoded = acc_decoded;
-      // 다음 미들웨어 실행
-      next();
-    }
-  });
-};
+    jwt.verify(access_token, process.env.ACCESS_TOKEN_KEY,(err, acc_decoded)=>{
+        if(err){
+            res.send("다시 로그인 해주세요");
+        }else{
+            req.acc_decoded = acc_decoded;
+            // 다음 미들웨어 실행
+            next();
+        }
+    })
+}
